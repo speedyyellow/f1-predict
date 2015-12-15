3a3bbfb8203afa665f9ea0470cf4d3e12975cf9a
Jon Evans <jonevans@liquidcapital.com>
2015-12-15 09:07:47 +0000

    Resolved #24 - using url reverse lookup

--- F1/urls.py
+++ F1/urls.py
@@ -26,6 +26,20 @@ urlpatterns = [
     url('^register/', CreateView.as_view(
                 template_name='registration/register.html',
                 form_class=UserCreationForm,
-                success_url='/accounts/login/?next=/predict'
-        )),
+                success_url='/accounts/login/?next=/'),
+                                name = 'register'
+    ),
 ]
+
+# django.contrib.auth.urls
+# urlpatterns = [
+#     url(r'^login/$', views.login, name='login'),
+#     url(r'^logout/$', views.logout, name='logout'),
+#     url(r'^password_change/$', views.password_change, name='password_change'),
+#     url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
+#     url(r'^password_reset/$', views.password_reset, name='password_reset'),
+#     url(r'^password_reset/done/$', views.password_reset_done, name='password_reset_done'),
+#     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+#         views.password_reset_confirm, name='password_reset_confirm'),
+#     url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
+# ]
